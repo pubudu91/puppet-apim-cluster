c3f2d0f92a7b9f9bbf9588dcf86269fd31173bb7
suhand <suhand85@gmail.com>
2015-09-14 12:22:02 +0000

    migration support

---
+++ modules/migration/manifests/apimgt18to19.pp
@@ -0,0 +1,35 @@
+class migration::apimgt18to19($rootUser,$rootPassword,$user,$password,$host,$apimgtdb,$userdb,$regdb)
+{
+
+staging::deploy { 'wso2am-1.9.0.zip':
+  source => 'puppet:///files/packs/apimanager/1.9.0/wso2am-1.9.0.zip',
+  target => '/tmp/',
+}
+
+exec { "delete-db":
+  command => "/usr/bin/mysql -u$rootUser -p$rootPassword -e \"DROP DATABASE apimgtdb;DROP DATABASE regdb;DROP DATABASE userdb;\"",
+}
+
+mysql::db { 'apimgtdb':
+    charset        => 'latin1',
+    collate        => 'latin1_swedish_ci',
+    user  => $user,
+    password => $password,
+    host     => $host,
+    sql => $apimgtdb,
+ }
+
+mysql::db { 'userdb':
+    user  => $user,
+    password => $password,
+    host     => $host,
+    sql => $userdb,
+ }
+
+mysql::db { 'regdb':
+    user  => $user,
+    password => $password,
+    host     => $host,
+    sql => $regdb,
+ }
+
