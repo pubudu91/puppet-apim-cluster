37aeb4d27b5e457b080078edd3b09f62d6559e72
suhand <suhand85@gmail.com>
2015-11-16 11:42:16 +0000

    nginx plus support for LBs

--- environments/production/modules/wso2base/manifests/loadbalancer.pp
+++ environments/production/modules/wso2base/manifests/loadbalancer.pp
@@ -1,6 +1,15 @@
 node /loadbalancer/ {
 
-        include 'nginx'
+        $loadbalancer = hiera("loadbalancer")
+
+        if $loadbalancer == 'nginx-plus' {
+            include 'nginx_plus'
+            notify {" ***************** Load balancer set to Nginx Plus. ***************** ": }
+        }
+        else {
+            include 'nginx'
+            notify {" ***************** Load balancer set to Nginx CE. ***************** ": }
+        }
 
         $dirpath = '/etc/nginx/ssl'
 
@@ -15,11 +24,16 @@ node /loadbalancer/ {
         }
         ->
         file { "/etc/nginx/conf.d/apimanger.conf":
-            notify  => Service["nginx"],  # this sets up the relationship
+            #notify  => Service["nginx"],  # this sets up the relationship
             owner   => root,
             group   => root,
             mode    => 775,
             content => template("wso2base/nginx.erb"),
+        }
+        ->
+        exec { "restart_nginx":
+                path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+                command => "service nginx restart",
         }
 
 }
